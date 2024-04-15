6babecb023d4ced8fd3bb7fcc2a3cdcb00260de0
haneul529 <lucia00529@skuniv.ac.kr>
2024-04-15 09:20:32 +0900

    알고리즘 수업 복습

---
+++ Java_Algorithm/src/main/java/com/algorithm/Problem.java
@@ -0,0 +1,64 @@
+package com.algorithm;
+
+// 자동완성 연습
+public class Problem {
+
+    // main
+    public static void main(String[] args) {
+        // sout
+        System.out.println();
+        int a = 3;
+        // soutv : 가까운 변수를 잡아서 디버깅 문 자동생성
+        System.out.println("a = " + a);
+        // souf
+        System.out.printf("");
+        // fori : 기본적인 for문 생성 (i 최대 범위는 직접 적어넣어야 함)
+        //for (int i = 0; i <; i++) {
+        //
+        //}
+        // iter : 향상된 for 구문 생성
+        for (String arg : args) {
+
+        }
+        /*
+         * 유용한 단축키 모음
+         *
+         * Ctrl + Shift + ↑, ↓
+         * ↪ 코드 블록 전체 이동
+         *
+         * Alt + Shift + ↑, ↓
+         * ↪ 해당 줄만 아래위로 이동(자리바꿈)
+         *
+         * Ctrl + Alt + M
+         * ↪ 해당 블록을 메서드로 분리
+         *   // 예시
+         *   public class Problem{
+         *       public static void main(String[] args) {
+         *           extracted();    // 해당 부분으로 분리
+         *       }
+         *
+         *       private static void extracted() {
+         *           int a = 3;
+         *       }
+         *   }
+         *
+         * Ctrl + D
+         * ↪ 해당 줄 복제
+         *
+         * Ctrl + E
+         * ↪ 직전 file 내역 탐색
+         *
+         * Shift + F6
+         * ↪ Refactor(변수 이름 일괄 변경)
+         *
+         * Ctrl + Alt + L
+         * ↪ 스타일 가이드 자동 적용
+         *
+         * 실행 단축키
+         * ↪ Ctrl + Shift + F10
+         *
+         * */
+    }
+
+}
+
